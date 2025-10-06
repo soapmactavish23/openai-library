7cd57ea6d748a1498ed7cf81a835b26d6b2d28a9
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-06 00:18:48 -0300

    first commit

---
+++ src/main/java/br/com/housesoftware/openailibrary/OpenaiLibraryApplication.java
@@ -0,0 +1,13 @@
+package br.com.housesoftware.openailibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenaiLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/housesoftware/openailibrary/OpenaiLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.housesoftware.openailibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaiLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
